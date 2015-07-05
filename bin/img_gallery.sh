9621c723fc76b7bb9976aaac43ab6d03e03ba88d
TJ Vanderpoel <tj@rubyists.com>
2015-07-05 16:16:51 -0500

    more stuff in img_gallery.sh

--- bin/img_gallery.sh
+++ bin/img_gallery.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+gallery.sh -i "$@" > index.html
 gallery.sh -c2 "$@" > ul.html
 gallery.sh -c4 "$@" > ul-m.html
 gallery.sh -c6 "$@" > ul-lg.html
+[ -d thumbs ] || mkdir thumbs
+gallery.sh -m
