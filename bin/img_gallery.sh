7623be936ea12836ad320536a78a3644f2e31d93
TJ Vanderpoel <tj@rubyists.com>
2015-07-06 11:29:26 -0500

    fixes

--- bin/img_gallery.sh
+++ bin/img_gallery.sh
@@ -4,4 +4,4 @@ gallery.sh "$@"     > ul.html    # small layout
 gallery.sh -c4 "$@" > ul-m.html  # medium layout
 gallery.sh -c6 "$@" > ul-lg.html # large layout
 [ -d thumbs ] || mkdir thumbs    # Make thumbnail dir
-gallery.sh -m                    # Generate thumbnails
+gallery.sh -m "$@"               # Generate thumbnails
